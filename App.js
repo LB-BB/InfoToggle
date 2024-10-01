a4416634c6ce6b2529787abcab8184c936540f62
LB-BB <57300241+LB-BB@users.noreply.github.com>
2024-10-01 15:53:55 -0700

    style & submission rules

--- App.js
+++ App.js
@@ -14,44 +14,80 @@ const App = () => {
   const [name, setName] = useState('');
   const [age, setAge] = useState('');
   const [shouldDisplayText, setShouldDisplayText] = useState(true);
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const getDisplayText = () => {
+    return `Hello, ${name}! Congratulations on making it around the sun ${age} times.`;
+  };
+
+  const handleSubmit = () => {
+    if (name && age) {
+      setIsSubmitted(true);
+      setShouldDisplayText(true);
+    } else {
+      setIsSubmitted(false);
+      setShouldDisplayText(false);
+    }
+  };
+
+  const handleNamechange = (value) => {
+    setName(value);
+    setIsSubmitted(false);
+    setShouldDisplayText(false);
+  };
+
+  const handleAgechange = (value) => {
+    setAge(value);
+    setIsSubmitted(false);
+    setShouldDisplayText(false);
+  };
+
   return (
-    <SafeAreaView>
-      <View>
-        <TextInput
-          style={style.inputs}
-          value={name}
-          onChangeText={(value) => {
-            setName(value);
-          }}
-          placeholder="Please enter your name"
-        />
-        <TextInput
-          style={style.inputs}
-          value={age}
-          onChangeText={(value) => {
-            setAge(value);
-          }}
-          placeholder="Please enter your age"
-        />
-      </View>
-      <TouchableOpacity
-        style={[{backgroundColor: 'black'}, (name.length === 0 && {opacity: 0.5})]}>
-        <View>
-          <Text style={{color: 'white', textAlign: 'center', padding: 10}}>
-            Submit
-          </Text>
+
+      <SafeAreaView style={style.container}>
+        <View style={style.title}>
+          <Text style={style.titleText}>Input Form</Text>
         </View>
-      </TouchableOpacity>
-      <Switch value={shouldDisplayText} onValueChange={(value) => {
-        setShouldDisplayText(value);
-      }} />
-      <Text>Display information below?</Text>
-      <ScrollView>
-        <Text>
-          {shouldDisplayText === true ? `${name}, ${age}` : ''}
-        </Text>
-      </ScrollView>
-    </SafeAreaView>
+        <View>
+          <TextInput
+            style={style.input}
+            value={name}
+            onChangeText={handleNamechange}
+            placeholder="Please enter your name"
+          />
+          <TextInput
+            style={style.input}
+            value={age}
+            onChangeText={handleAgechange}
+            placeholder="Please enter your age"
+          />
+        </View>
+        <TouchableOpacity
+          style={[style.submitButton, (name.length === 0 || age.length === 0) && {opacity: 0.5}]}
+          disabled={name.length === 0 || age.length === 0}
+          onPress={handleSubmit}
+        >
+          <View>
+            <Text style={style.submitText}>
+              Submit
+            </Text>
+          </View>
+        </TouchableOpacity>
+        <View style={style.switch}>
+          <Switch
+          value={shouldDisplayText}
+          onValueChange={(value) => {
+            setShouldDisplayText(value);
+          }}
+          />
+          <Text style={style.switchText}>Display information below?</Text>
+        </View>
+        <ScrollView style={style.displayView}>
+          <Text style={style.displayViewText}>
+            {shouldDisplayText && isSubmitted ? getDisplayText() : ''}
+          </Text>
+        </ScrollView>
+      </SafeAreaView>
   );
 };
 
--- style.js
+++ style.js
@@ -1,8 +1,54 @@
 const style = {
-  inputs: {
+  container: {
+    flex: 1,
+    padding: 16,
+    backgroundColor: '#C696D3',
+  },
+  title: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 20,
+  },
+  titleText: {
+    fontSize: 35,
+    fontWeight: 'bold',
+    fontFamily: 'Courier New',
+  },
+  input: {
     borderWidth: 1,
     padding: 10,
     borderRadius: 4,
+    marginHorizontal: 10,
+    marginTop: 10,
+    fontSize: 16,
+    backgroundColor: 'white',
+  },
+  submitButton: {
+    backgroundColor: 'black',
+    padding: 10,
+    marginHorizontal: 10,
+    marginTop: 10,
+  },
+  submitText: {
+    color: 'white',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  switch: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 10,
+  },
+  switchText: {
+    marginHorizontal: 10,
+    fontSize: 16,
+  },
+  displayView: {
+    marginHorizontal: 10,
+  },
+  displayViewText: {
+    fontSize: 30,
+    fontFamily: 'Courier New',
   },
 };
 export default style;
